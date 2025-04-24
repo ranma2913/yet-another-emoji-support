c3fdbb4907942de5bbbdee1cc0c9614a9f9c973c
Joel Sticha (ranma2913) <4295880+ranma2913@users.noreply.github.com>
2025-04-24 11:46:48 -0500

    :arrow_up: Increase `untilBuild` to `251.*`

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("243.*")
+        untilBuild.set("251.*")
         changeNotes.set(project.file("LATEST.txt").readText())
     }
 
